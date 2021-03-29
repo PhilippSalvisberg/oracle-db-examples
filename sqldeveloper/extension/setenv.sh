5547d4155012de2cfbe8195f54a71be06bed1216
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-03-29 14:44:20 +0200

    make it work

--- sqldeveloper/extension/setenv.sh
+++ sqldeveloper/extension/setenv.sh
@@ -1,6 +1,7 @@
-export ANT_HOME=/usr/local/Cellar/ant/1.10.5
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_221.jdk/Contents/Home
-export ANT_BIN=${ANT_HOME}/bin
-export SQLDEV_BIN=/Users/bjeffrie/eclipse-workspace-20.1/sql-developer/ide/sqldeveloper/bin
+#export ANT_HOME=/usr/local/Cellar/ant/1.10.5
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_221.jdk/Contents/Home
+#export ANT_BIN=${ANT_HOME}/bin
+#export SQLDEV_BIN=/Users/bjeffrie/eclipse-workspace-20.1/sql-developer/ide/sqldeveloper/bin
+export SQLDEV_BIN=/Applications/SQLDeveloper20.2.0.app/Contents/Resources/sqldeveloper
 export PATH=${PATH}:${ANT_BIN}:${SQLDEV_BIN}
 
